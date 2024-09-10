8ad0414273f4ced319f7b8234299511a115d840d
laon6 <laon6.dev@gmail.com>
2024-09-10 18:19:10 +0900

    테스트용

--- api/cron.js
+++ api/cron.js
@@ -12,7 +12,8 @@ export default async function handler(req, res) {
         day: 'numeric',
     });
 
-    const message = `[${today}] <!channel> \n슬랙 확인 후 확인완료 댓글 달아주세요.`;
+    const message = `[${today}] 테스트용`;
+    //<!channel> \n슬랙 확인 후 확인완료 댓글 달아주세요.
 
     try {
         await axios.post('https://slack.com/api/chat.postMessage', {
