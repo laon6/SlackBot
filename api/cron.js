53ee8aa83e82dcbdec5fa60c75f3216086faf0f2
laon6 <laon6.dev@gmail.com>
2024-09-19 11:40:41 +0900

    알림 중지

--- api/cron.js
+++ api/cron.js
@@ -1,35 +1,35 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    if (req.headers['authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
-        return res.status(401).end('Unauthorized');
-    }
+    // if (req.headers['authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
+    //     return res.status(401).end('Unauthorized');
+    // }
 
-    const today = new Date().toLocaleDateString('ko-KR', {
-        timeZone: 'Asia/Seoul',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    });
+    // const today = new Date().toLocaleDateString('ko-KR', {
+    //     timeZone: 'Asia/Seoul',
+    //     year: 'numeric',
+    //     month: 'long',
+    //     day: 'numeric',
+    // });
 
     const message = `[${today}] 테스트용`;
     //<!channel> \n슬랙 확인 후 확인완료 댓글 달아주세요.
 
-    try {
-        await axios.post('https://slack.com/api/chat.postMessage', {
-            channel: process.env.CHANNEL_ID,
-            text: message,
-            thread_ts: process.env.THREAD_TS,
-        }, {
-            headers: {
-                Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-                'Content-Type': 'application/json',
-            },
-        });
+    // try {
+    //     await axios.post('https://slack.com/api/chat.postMessage', {
+    //         channel: process.env.CHANNEL_ID,
+    //         text: message,
+    //         thread_ts: process.env.THREAD_TS,
+    //     }, {
+    //         headers: {
+    //             Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+    //             'Content-Type': 'application/json',
+    //         },
+    //     });
 
-        res.status(200).end('Message sent!');
-    } catch (error) {
-        console.error(error);
-        res.status(500).end('Error sending message');
-    }
+    //     res.status(200).end('Message sent!');
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).end('Error sending message');
+    // }
 }
